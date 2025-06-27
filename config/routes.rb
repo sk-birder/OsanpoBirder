9221c00e5f046862d075cee07db9803d5096d527
sk-birder <199427351+sk-birder@users.noreply.github.com>
2025-06-27 01:03:25 +0000

    [Edit] エンドユーザのサインアップ画面を編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
-  
-  root to: 'public/homes#top'
-  get 'about' => 'public/homes#about'
+  # エンドユーザ側
+  scope module: :public do
+    devise_for :users
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+  end
+
+  # 管理者側
+  devise_for :admin, controllers: {
+    registrations: 'admin/registrations',
+    sessions: 'admin/sessions'
+  }
+
+  # namespace :admin do
+  # end
 end
