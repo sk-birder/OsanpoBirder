0798b7788dcde53d870ebf0c0a0510654e2699f8
sk-birder <199427351+sk-birder@users.noreply.github.com>
2025-07-07 09:16:07 +0000

    [Add] サインイン機能に退会済みユーザーの判定を追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  # is_active判定
+  before_action :user_status, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -31,4 +33,19 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  private
+  # is_active判定用のメソッド
+  def user_statusg
+    # 入力されたメールアドレスがUserテーブルにあるか確認して、無ければdeviseに返して拒否してもらう
+    user = User.find_by(email: params[:user][:email])
+    return if user.nil?
+    # パスワードが一致しているか確認して、無ければdeviseに返して拒否してもらう
+    return unless user.valid_password?(params[:user][:password])
+    # is_activeがfalseのときdeviseの処理を中断してサインアップ画面に遷移する
+    if user.is_active == false
+      flash[:notice] = '退会済みのアカウントです。'
+      redirect_to new_user_registration_path
+    end
+  end
 end
