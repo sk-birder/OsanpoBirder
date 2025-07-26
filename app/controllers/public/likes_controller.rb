97889c97e43c245bcea3848ac47cc749ed2be6a9
sk-birder <199427351+sk-birder@users.noreply.github.com>
2025-07-26 16:02:44 +0000

    [Add] いいね一覧を追加

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,6 +1,6 @@
 class Public::LikesController < ApplicationController
   before_action :authenticate_user!
-  before_action :ensure_guest_user
+  before_action :ensure_guest_user, except: [:index]
   before_action :deny_deactivated_user
   
   def index
@@ -15,14 +15,14 @@ class Public::LikesController < ApplicationController
     post = Post.find(params[:post_id])
     like = current_user.likes.new(post_id: post.id)
     like.save
-    redirect_to post_path(post)
+    redirect_back fallback_location: root_path
   end
 
   def destroy
     post = Post.find(params[:post_id])
     like = current_user.likes.find_by(post_id: post.id)
     like.destroy
-    redirect_to post_path(post)
+    redirect_back fallback_location: root_path
   end
 
   private
