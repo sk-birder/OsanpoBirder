97f99f90addf88206198ff1262879102bb9e3b4d
sk-birder <kibe.shuhei@gmail.com>
2026-03-11 23:36:04 +0900

    [Change] ユーザー詳細のテンプレートにおける都道府県の表示方法を変更

--- app/helpers/public/users_helper.rb
+++ app/helpers/public/users_helper.rb
@@ -6,4 +6,18 @@ module Public::UsersHelper
       '都道府県非公開'
     end
   end
+
+  def display_prefecture_data(user)
+    if user.hide_prefecture
+      {
+        css: 'text-white-50',
+        text: hidden_prefecture_display(user)
+      }
+    else
+      {
+        css: 'text-white',
+        text: user.prefecture
+      }
+    end
+  end
 end
