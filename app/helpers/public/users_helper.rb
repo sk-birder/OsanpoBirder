b44686c0970e65d8330919f9bf739b64ded3a5d5
sk-birder <kibe.shuhei@gmail.com>
2026-03-13 23:49:39 +0900

    [Change] ユーザー詳細の誕生年の表示方式を都道府県と同様の方式に変更

--- app/helpers/public/users_helper.rb
+++ app/helpers/public/users_helper.rb
@@ -1,23 +1,29 @@
 module Public::UsersHelper
-  def hidden_prefecture_display(user)
-    if user == current_user
-      "#{user.prefecture}(非公開)"
+  def prefecture_display_data(user)
+    if user.hide_prefecture
+      text = 
+        if user == current_user
+          "#{user.prefecture}(非公開)"
+        else
+          '都道府県非公開'
+        end
+      { css: 'text-white-50', text: text }
     else
-      '都道府県非公開'
+      { css: 'text-white', text: user.prefecture }
     end
   end
 
-  def display_prefecture_data(user)
-    if user.hide_prefecture
-      {
-        css: 'text-white-50',
-        text: hidden_prefecture_display(user)
-      }
+  def birth_year_display_data(user)
+    if user.hide_birth_year
+      text = 
+        if user == current_user
+          "#{user.birth_year}年(非公開)"
+        else
+          '誕生年非公開'
+        end
+      { css: 'text-white-50', text: text }
     else
-      {
-        css: 'text-white',
-        text: user.prefecture
-      }
+      { css: 'text-white', text: "#{user.birth_year}年" }
     end
   end
 end
